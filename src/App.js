e75274054dd6bf4f86063647eaa96fb7a0ef216e
unknown <n1210443@JN2.IWASAKI.AC.JP>
2023-08-06 22:09:41 +0900

    station1,2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 const BASE_URL = 'https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com';
-
-const ThreadList = ({ threads }) => {
-  return (
-    <div>
-      <h1>スレッド一覧</h1>
-      <ul>
-        {threads.map((thread) => (
-          <li key={thread.id}>
-            <h3>{thread.title}</h3>
-            <p>{thread.content}</p>
-          </li>
-        ))}
-      </ul>
-      <Link to="/thread/new">新規スレッド作成</Link>
-    </div>
-  );
-};
-
-const NewThread = () => {
-  return (
-    <div>
-      <h1>スレッド新規作成</h1>
-      {/* ここに新規スレッド作成のフォームを追加 */}
-    </div>
-  );
-};
 
 const App = () => {
   const [threads, setThreads] = useState([]);
@@ -48,16 +21,17 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <ThreadList threads={threads} />
-        </Route>
-        <Route path="/thread/new">
-          <NewThread />
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <h1>スレッド一覧</h1>
+      <ul>
+        {threads.map((thread) => (
+          <li key={thread.id}>
+            <h3>{thread.title}</h3>
+            <p>{thread.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
